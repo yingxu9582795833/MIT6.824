0fc6ab879ecef19bfeeb55b6ce9d5c8c222e36b5
yingxu95827 <2581204412@qq.com>
2022-11-27 15:59:28 +0800

    向util.go工具类中添加了随机选举时间函数

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,6 +1,10 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"math/rand"
+	"time"
+)
 
 // Debugging
 const Debug = false
@@ -10,4 +14,9 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 		log.Printf(format, a...)
 	}
 	return
+}
+
+//选举的随机时间
+func RandElection() time.Duration {
+	return time.Duration(rand.Int()%250+150) * time.Millisecond
 }
